054df5e296d6366aa4fd02d52a05abb9d34422f7
riomonet <arizablozki@gmail.com>
2023-03-31 22:49:08 -0400

    add 07_multiple_letter_count

--- python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
+++ python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,12 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    p = set(phrase)
+    dict = {}
+    for letter in p :
+        count = phrase.count(letter)
+        dict[letter] = count
+    return dict
+    
+
+
