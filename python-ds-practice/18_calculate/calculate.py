43f98e3b463a1ac72130f2474ec36e7ec7e7b530
riomonet <arizablozki@gmail.com>
2023-04-01 22:08:05 -0400

    add solution provided as commeenet to ex 18

--- python-ds-practice/18_calculate/calculate.py
+++ python-ds-practice/18_calculate/calculate.py
@@ -43,3 +43,21 @@ def calculate(operation, a, b, make_int=False, message='The result is'):
     else:
         return
 
+#solution below was better than mine above
+
+
+    #     if operation == "add":
+    #     res = a + b
+    # elif operation == "subtract":
+    #     res = a - b
+    # elif operation == "multiply":
+    #     res = a * b
+    # elif operation == "divide":
+    #     res = a / b
+    # else:
+    #     return
+
+    # if make_int:
+    #     res = int(res)
+
+    # return f"{message} {res}"
